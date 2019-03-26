c75cdce4743278dfe20372f5faa543b72c2871f3
Chris <chris.m.hein@gmail.com>
2019-03-26 08:44:43 -0400

    fixed ESLint issues

--- models/Article.js
+++ models/Article.js
@@ -1,7 +1,7 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose')
 
 // Save a reference to the Schema constructor
-var Schema = mongoose.Schema;
+var Schema = mongoose.Schema
 
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
@@ -21,12 +21,12 @@ var ArticleSchema = new Schema({
   // This allows us to populate the Article with an associated Note
   note: {
     type: Schema.Types.ObjectId,
-    ref: "Note"
+    ref: 'Note'
   }
-});
+})
 
 // This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
+var Article = mongoose.model('Article', ArticleSchema)
 
 // Export the Article model
-module.exports = Article;
+module.exports = Article
